ae2596ea345a69536d6f3527ad11dc99e7320984
Nata <erbet@mail.ru>
2023-12-01 18:12:06 +0300

    add thunk

--- src/project/store/index.ts
+++ src/project/store/index.ts
@@ -1,5 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createAPI } from '../service/api';
 
-const store = configureStore({ reducer });
+const api = createAPI();
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument : api,
+    },
+  })
+});
+
+
 export { store };
