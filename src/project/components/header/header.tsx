224dddc040c806616b93bbccd9e43c2ffe56642d
Nata <erbet@mail.ru>
2023-12-03 21:03:45 +0300

    feat: add auth into header component

--- src/project/components/header/header.tsx
+++ src/project/components/header/header.tsx
@@ -1,72 +1,52 @@
-import { Link, useLocation } from 'react-router-dom';
-import classNames from 'classnames';
-import { AppRoute } from '../../const/const';
+import { Link } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const/const';
 import { useAppSelector} from '../../hooks';
-import { RequestStatus } from '../../const/const';
+import { Logo } from '../../components/logo/logo';
 
-type HeaderProps ={
-  hideNavigation?: boolean;
-}
+function Header() {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
 
-function Header({
-  hideNavigation = false,
-}: HeaderProps) {
-
-  const { pathname } = useLocation();
-  const authStatus = useAppSelector((state) => state.authorizationStatus);
   return(
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
             <Link
-              className= {classNames(
-                'header__logo-link',
-                {
-                  'header__logo-link--active':pathname === AppRoute.Main //хз?
-                }
-              )}
+              className='header__logo-link'
               to={AppRoute.Main}
             >
-              <img
-                className="header__logo"
-                src="img/logo.svg"
-                alt="6 cities logo"
-                width="{81}"
-                height="{41}"
-              />
+              <Logo />
             </Link>
 
           </div>
-          { !hideNavigation && (
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                {authStatus === RequestStatus.Error && (
-                  <li className="header__nav-item user">
-                    <Link
-                      className="header__nav-link header__nav-link--profile"
-                      to={AppRoute.Favorites}
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper" />
-                      <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                      </span>
-                      <span className="header__favorite-count">3</span>
-                    </Link>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                {authorizationStatus === AuthorizationStatus.Auth
+                  ?
+                  <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile" >
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__favorite-count">3</span>
+                  </Link>
+                  :
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__login">Sign in</span>
+                  </Link>}
+              </li>
+              {authorizationStatus === AuthorizationStatus.Auth
+                &&
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>}
 
-                  </li>
-                )}
-                {authStatus === RequestStatus.Success && (
-                  <li className="header__nav-item">
-                    <Link className="header__nav-link" to={AppRoute.Login}>
-                      <span className="header__signout">Sign out</span>
-                    </Link>
-
-                  </li>
-                )}
-              </ul>
-            </nav>
-          )}
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
--- src/project/pages/login-page/login-page.tsx
+++ src/project/pages/login-page/login-page.tsx
@@ -1,43 +1,59 @@
 import { Link } from 'react-router-dom';
-
+import { Helmet } from 'react-helmet-async';
+import { Header } from '../../components/header/header';
 function LoginPage() {
   return (
-    <div className="page__login-container container">
-      <section className="login">
-        <h1 className="login__title">Sign in</h1>
-        <form className="login__form form" action="#" method="post">
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">E-mail</label>
-            <input
-              className="login__input form__input"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-            />
+    <div className="page page--gray page--login">
+      <Helmet>
+        <title>{'6 cities - Login'}</title>
+      </Helmet>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo />
+            </div>
           </div>
-          <div className="login__input-wrapper form__input-wrapper">
-            <label className="visually-hidden">Password</label>
-            <input
-              className="login__input form__input"
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-          </div>
-          <button className="login__submit form__submit button" type="submit">
-        Sign in
-          </button>
-        </form>
-      </section>
-      <section className="locations locations--login locations--current">
-        <div className="locations__item">
-          <Link className="locations__item-link" to="#">
-            <span>Amsterdam</span>
-          </Link>
         </div>
-      </section>
+      </header>
+
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <form className="login__form form" action="#" method="post">
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">E-mail</label>
+              <input
+                className="login__input form__input"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">Password</label>
+              <input
+                className="login__input form__input"
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button className="login__submit form__submit button" type="submit">
+          Sign in
+            </button>
+          </form>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <Link className="locations__item-link" to="#">
+              <span>Amsterdam</span>
+            </Link>
+          </div>
+        </section>
+      </div>
     </div>
 
   );
