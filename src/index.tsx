04b6214df8d0fa3b038e080f405ffcd18fee4b60
Nata <erbet@mail.ru>
2023-12-04 09:59:36 +0300

    feat: add react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './project/store/index';
 import { fetchOffers, checkAuth } from './project/store/api-actions';
 import App from './project/components/app/app';
+import 'react-toastify/dist/ReactToastfy.css';
+import { ToastContainer } from 'react-toastify';
 
 store.dispatch(fetchOffers);
 store.dispatch(checkAuth);
@@ -15,7 +17,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+    <ToastContainer />
+      <App />
     </Provider>
   </React.StrictMode>
 );
